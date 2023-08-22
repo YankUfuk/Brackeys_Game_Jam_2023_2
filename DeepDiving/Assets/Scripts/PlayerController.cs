e552187ee632dd3a3e01d8f3ba23dac12d2456b6
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-08-22 11:54:45 +0300

    Movement added

--- DeepDiving/Assets/Scripts/PlayerController.cs
+++ DeepDiving/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveForce = 10f;
+    private float movementX;
+    private float movementY;
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private float gravity = -0.1f;
+
+    private void Awake()
     {
-        
+
+        myBody = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
+
+        transform.position += new UnityEngine.Vector3(movementX, movementY + gravity, 0f) * Time.deltaTime * moveForce;
     }
 }
